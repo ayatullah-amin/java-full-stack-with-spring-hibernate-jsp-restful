f13002a2c8f6c55b7f9055604c4fb3ebf85e8313
ayatullah-amin <ayatullah.amin@gmail.com>
2021-03-14 15:15:33 +0700

    change code

--- S03L04 - Understanding variables/src/Hello.java
+++ S03L04 - Understanding variables/src/Hello.java
@@ -2,8 +2,12 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int value1;
+		value1 = 1;
+		int value2 = (2*2) - 1;
+		int value3 = 2, value4 = 2;
+		int value5 = value1*value2*value3;
+		System.out.println(value5);
 	}
 
 }

26f70bfbc9c0750551511e0be8fa2bb9890f5894
ayatullah-amin <ayatullah.amin@gmail.com>
2021-03-15 11:15:08 +0700

    add S04L01

---
+++ S04L01 - Arithmetic operators/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int x = 1 + 2; //adding
+		String s = "Hello" + "world"; //concatenation
+		int y = 12-x;
+		int z = 12/2;
+		int a = 12 % 2;
+		int b = 13 % 3;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(s);
+		System.out.println(z);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
